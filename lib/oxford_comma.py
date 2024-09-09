14b09b36ccbdcf895f09786c2adc431da3840333
emmanuel ronoh <eronoh036@gmail.com>
2024-09-09 16:34:36 +0300

    first commit

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,13 @@
+# oxford_comma.py
+
 def oxford_comma(items):
-    return None
+    '''Formats a list of items according to the Oxford comma rules.'''
+    if len(items) == 1:
+        return items[0]
+    elif len(items) == 2:
+        return f"{items[0]} and {items[1]}"
+    elif len(items) == 3:
+        return f"{items[0]}, {items[1]}, and {items[2]}"
+    else:
+        # For more than three elements
+        return ', '.join(items[:-1]) + ', and ' + items[-1]
